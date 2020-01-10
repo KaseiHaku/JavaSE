ddaab634b88ea323f53744a1014e97726e812219
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-01-11 03:08:21 +0800

    Update ClassInit2.java

--- src/kasei/javase/object_oriented/classinit/ClassInit2.java
+++ src/kasei/javase/object_oriented/classinit/ClassInit2.java
@@ -16,17 +16,24 @@ public class ClassInit2 extends ClassInit {
 
     public String str2 = "子类：4、动态变量加载执行...";
 
-    {
+    
+    public void print22(){
         System.out.println(str2);
-        System.out.println("子类：5、动态语句块加载执行...");
+        System.out.println("子类：5、动态函数加载执行...");
+    }
+    
+    {
+        print22();
+        System.out.println("子类：6、动态语句块加载执行...");
     }
 
-    public void print22(){
-        System.out.println("子类：6、动态函数加载执行...");
-    }
 
     public ClassInit2(){
-        print22();
         System.out.println("子类：7、构造函数加载执行...");
+    }
+    
+    public static void main(String[] args) {
+        new ClassInit2();
+        System.out.println("子类：演示完毕");
     }
 }
