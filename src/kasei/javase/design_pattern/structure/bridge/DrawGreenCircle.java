761fe7e77243d3732d93a16da9ac289fdd4ee532
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-01-19 14:20:11 +0800

    Delete DrawGreenCircle.java

--- src/kasei/javase/design_pattern/structure/bridge/DrawGreenCircle.java
+++
@@ -1,9 +0,0 @@
-package kasei.javase.designpattern.structure.bridge;
-
-public class DrawGreenCircle implements DrawAPI {
-
-	@Override
-	public void drawCircle() {
-		System.out.println("绿圆");	
-	}	
-}
