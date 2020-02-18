52d2d781fc26814c9724b3678ed4b18d65960c7f
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-02-18 19:40:34 +0800

    Update ThreadCreate.java

--- src/kasei/javase/thread/ThreadCreate.java
+++ src/kasei/javase/thread/ThreadCreate.java
@@ -56,19 +56,35 @@ public class ThreadCreate {
                 e.printStackTrace();
             }
         };
-        // todo 创建线程池
-        ExecutorService executor = new ThreadPoolExecutor(
-                2,
-                4,
-                100,
-                TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<>(8),
-                (task) -> {
+        // TODO 创建线程池参数
+        int corePoolSize = 2; 
+        int maximumPoolSize = 4; 
+        long keepAliveTime = 100; 
+        TimeUnit unit = TimeUnit.MILLISECONDS;  
+        BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(8);  
+        ThreadFactory threadFactory = (task) -> {       
                     Thread t1 = new Thread(task);
                     t1.setUncaughtExceptionHandler(handler);
                     return t1;
-                },
-                new ThreadPoolExecutor.AbortPolicy()
+                };
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.AbortPolicy(); 
+        
+        /** TODO 线程池总结
+         * 1. 如果当前线程池中的线程 < corePoolSize ，则每来一个任务，就会创建一个线程去执行这个任务，不管线程池中是否有空闲线程
+         * 2. 如果当前线程池中的线程 >= corePoolSize，则每来一个任务，尝试添加该任务到缓存队列中，
+         *    若成功，则等待空闲线程来将其取出并执行；若失败（队列已满），则尝试创建新的线程取执行这个任务
+         * 3. 如果当前线程池中的线程数目达到 maximumPoolSize，则会采取任务拒绝策略进行处理
+         * 4. 如果线程池中的线程 > corePoolSize 时，如果某线程空闲时间超过 keepAliveTime ，线程将被终止，直至线程池中线程数目不大于 corePoolSize
+         *    如果允许为核心线程设置存活时间，那么核心线程空闲时间超过 keepAliveTime，核心线程也会被终止
+         * */
+        ExecutorService executor = new ThreadPoolExecutor(
+                int corePoolSize,                       // 核心线程数
+                int maximumPoolSize,                    // 最大线程数
+                long keepAliveTime,                     // 空闲线程最大存活时间
+                TimeUnit unit,                          // 存活时间的单位
+                BlockingQueue<Runnable> workQueue,      // 阻塞队列
+                ThreadFactory threadFactory,            // 线程工厂
+                RejectedExecutionHandler handler        // 拒绝策略
         );
 
         // todo 实现 Callable 接口
