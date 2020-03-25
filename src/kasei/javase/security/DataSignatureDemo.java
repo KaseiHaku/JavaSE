a8889fdd4e8f931bcf235b45b53b54c6ff3fa669
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-03-25 09:21:35 +0800

    Update DataSignatureDemo.java

--- src/kasei/javase/security/DataSignatureDemo.java
+++ src/kasei/javase/security/DataSignatureDemo.java
@@ -1,4 +1,8 @@
 /** TODO Java 中的数据签名 */
 public class DataSignatureDemo {
-
+    try {
+        Signature signature = Signature.getInstance("SHA384withECDSA");
+    } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+    }
 }
