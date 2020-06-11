b2ddaad904b1e56fd212b6487add3e1277edc183
KSF <1291114093@qq.com>
2020-06-12 01:55:44 +0800

    ReactiveStream

---
+++ src/kasei/javase/juc/reactive/ReactiveStreamDemo.java
@@ -0,0 +1,26 @@
+package kasei.javase.juc.reactive;
+
+import java.util.concurrent.Flow;
+
+public class ReactiveStreamDemo {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        Flow.Publisher publisher = null;
+        Flow.Subscriber subscriber = null;
+        Flow.Subscription subscription = null;  // subscriber 通过该类去通知 publisher
+        Flow.Processor processor = null; // 中间处理人
+
+
+
+
+    }
+
+
+}
