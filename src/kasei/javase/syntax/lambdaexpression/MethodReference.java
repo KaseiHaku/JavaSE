13c6d264e4e80ac30685ea60d9d72c0d262473f8
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-07-14 02:21:57 +0800

    Update MethodReference.java

--- src/kasei/javase/syntax/lambdaexpression/MethodReference.java
+++ src/kasei/javase/syntax/lambdaexpression/MethodReference.java
@@ -10,17 +10,21 @@ public class MethodReference{
         comsumerLambda.accept("data");
         
         // method reference format; static method
+        // 没有 this 隐藏参数
         Comsumer<String> comsumerMethodReference = System.out::println;
         comsumerMethodReference.accept("data");
         
         
         // method reference format; instance method use instance
+        // 有 this 隐藏参数，但是 this 隐藏参数在 实例方法引用时，已经分配给 实例了，所以最后只剩一个参数
         String str = new String("1234");
-        Predicate predicate = str::equals;
+        Predicate predicate = str::equals; // 相当于 x -> str.equals(x);
         boolean b = Stream.of("a", "b", "c").anyMatch(predicate);
         System.out.println(b);
         
         // method reference format; instance method use Class
+        // 坑： java 每个方法中，都隐含一个 this 参数，指向当前对象，当使用 Class::instanceMethod 模式引用时，隐藏参数 this 会被替换成第一个参数
+        // 类实例方法 引用时，引用的方法其实有两个参数 (this, param1)
         BiFunction<String, String, Boolean> biFunction = String::equals;
         Boolean apply = biFunction.apply("sa", "sa");
         System.out.println(apply);
