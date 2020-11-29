a6d4c1462d1c5363231f7c3ff4a1d42115426c4b
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-11-29 21:13:57 +0800

    Update GenericClass.java

--- src/kasei/javase/syntax/generic/GenericClass.java
+++ src/kasei/javase/syntax/generic/GenericClass.java
@@ -3,5 +3,10 @@ public class GenericClass<T, U>{
     public <T, U> T method1(){
     }
   
+    public genericMethodInvoke(){
+        GenericClass.<String, Integer>method();  // 静态泛型方法调用时，可以在 className.<> 后加入泛型
+        this.<String, Integer>method();  // 类内部调用泛型方法时，可以在 this.<> 后加入反省
+        instance.<String, Integer>method(); // 通过实例调用泛型方法， instance.<>
+    }
 
 }
