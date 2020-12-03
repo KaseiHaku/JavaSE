98b752f792f5ff01ca5a27e964ecc4aa25c75081
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-12-04 02:12:45 +0800

    Update Test.java

--- src/kasei/javase/design_pattern/establish/factory_abstract/Test.java
+++ src/kasei/javase/design_pattern/establish/factory_abstract/Test.java
@@ -1,6 +1,14 @@
 package kasei.javase.designpattern.establish.f02AbstractFactory;
 
 
+
+/* 抽象工厂 跟 工厂 的区别
+工厂： 给个 参数 直接获取到 产品实例
+抽象工厂： 给个 参数  获取到的是  工厂的实例，  再给工厂实例一个参数，才能获取 产品实例
+
+
+*/
+
 //抽象工厂模式
 public class Test {
 	public static void main(String[] args){
