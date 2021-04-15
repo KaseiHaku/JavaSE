6b55a2d367b6c63b1d5ebf3d6bc47a68a2e634e1
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2021-04-15 10:30:44 +0800

    Update Test.java

--- src/kasei/javase/design_pattern/behavior/interpreter/Test.java
+++ src/kasei/javase/design_pattern/behavior/interpreter/Test.java
@@ -1,6 +1,11 @@
 package kasei.javase.designpattern.behavior.interpreter;
 
-//解释器模式：
+/** 解释器模式：
+ * Interpreter： 解释器自身
+ * Expression：表达式
+ * Context: 表达式上下文环境
+ * 应用实例: 编译器
+ * */
 public class Test {
 
 	public static void main(String[] args) {
