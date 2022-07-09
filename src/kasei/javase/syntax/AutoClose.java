4b0c46c3c4fe2c4458f23782539b18bf0ba49329
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2022-07-10 05:08:15 +0800

    Update AutoClose.java

--- src/kasei/javase/syntax/AutoClose.java
+++ src/kasei/javase/syntax/AutoClose.java
@@ -5,8 +5,9 @@ public class AutoClose implements AutoCloseable {
     public void autoCloseResource(){
         
         // 该语法自动关闭 try() 中的所有实现了 AutoCloseable 接口的实例
+        // @trap try() 中定义的 实例，默认为 final 修饰
         try(InputStream is = new ByteArrayInputStream();
-                OutputStream os = null) {
+                OutputStream os = new ByteArrayOutputStream()) {
             
             IOUtils.copy(is, os);
             
